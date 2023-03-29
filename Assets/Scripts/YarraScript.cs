9ce05e31cc37913fa04e63d8ba1665ad2cb3a617
DV <david_ventura_m@hotmail.com>
2023-03-29 09:35:13 -0400

    from school

---
+++ Assets/Scripts/YarraScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YarraScript : MonoBehaviour
+{
+    public static int exp;
+    public static int weaponDamage;
+
+    private void Start()
+    {
+        exp = 0;
+        weaponDamage = 0;
+    }
+
+    public void Dismount()
+    {
+
+    }
+
+    public void Travel()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+}
